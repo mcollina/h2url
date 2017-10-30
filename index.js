aea4dfa3490bff52dcb0be7c10a0c3d13f61f33d
Matteo Collina <hello@matteocollina.com>
2017-10-30 17:29:05 +0100

    core API

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const http2 = require('http2')
+const { URL } = require('url')
+const assert = require('assert')
+const getStream = require('get-stream')
+const pump = require('pump')
+const eos = require('end-of-stream')
+
+async function request (opts) {
+  assert.equal(typeof opts, 'object')
+
+  const url = new URL(opts.url)
+
+  const client = http2.connect(url.origin, {
+    rejectUnauthorized: false
+  })
+
+  const req = Object.assign({
+    ':path': url.pathname,
+    ':method': opts.method || 'GET'
+  }, opts.headers)
+
+  const stream = client.request(req)
+
+  end(stream, opts.body)
+
+  // Needed because of https://github.com/nodejs/node/issues/16617
+  // I would use unref() instead and let the user destroy the client
+  eos(stream, () => {
+    client.destroy()
+  })
+
+  const headers = await waitForHeaders(stream)
+
+  return { headers, stream }
+}
+
+async function concat (opts) {
+  const res = await request(opts)
+  const headers = res.headers
+  const body = await getStream(res.stream)
+
+  return { body, headers }
+}
+
+function waitForHeaders (req) {
+  return new Promise(function (resolve, reject) {
+    req.on('response', resolve)
+  })
+}
+
+function end (req, body) {
+  if (!body || typeof body === 'string' || body instanceof Uint8Array) {
+    req.end(body)
+  } else if (body.pipe) {
+    pump(body, req)
+  } else {
+    throw new Error(`type unsupported for body: ${body.constructor}`)
+  }
+}
+
+module.exports = {
+  request,
+  concat
+}
---
+++ test/basic.js
@@ -0,0 +1,160 @@
+'use strict'
+
+const { test } = require('tap')
+const fs = require('fs')
+const path = require('path')
+const http2 = require('http2')
+const h2url = require('..')
+const {
+  Readable,
+  Writable
+} = require('stream')
+const getStream = require('get-stream')
+
+test('https', (t) => {
+  const server = http2.createSecureServer({
+    key: fs.readFileSync(path.join(__dirname, 'test.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'test.cert'))
+  })
+
+  server.listen(0, () => {
+    server.unref()
+
+    doTests(server, 'https', t.test)
+    t.end()
+  })
+})
+
+test('http', (t) => {
+  const server = http2.createServer()
+
+  server.listen(0, () => {
+    server.unref()
+
+    doTests(server, 'http', t.test)
+    t.end()
+  })
+})
+
+function doTests (server, scheme, test) {
+  const url = `${scheme}://localhost:${server.address().port}`
+
+  test('concat request', async (t) => {
+    server.once('stream', (stream, headers) => {
+      t.equal(headers[':method'], 'GET')
+      stream.respond({ ':status': 200 })
+      stream.end('hello world')
+    })
+
+    const res = await h2url.concat({ url })
+
+    t.equal(res.body, 'hello world')
+    t.equal(res.headers[':status'], 200)
+  })
+
+  test('concat post', async (t) => {
+    server.once('stream', async (stream, headers) => {
+      t.equal(headers[':method'], 'POST')
+
+      const body = await getStream(stream)
+
+      t.equal(body, 'this is a post!')
+
+      stream.respond({ ':status': 200 })
+      stream.end('hello world')
+    })
+
+    const res = await h2url.concat({
+      url,
+      method: 'POST',
+      body: 'this is a post!'
+    })
+
+    t.equal(res.body, 'hello world')
+    t.equal(res.headers[':status'], 200)
+  })
+
+  test('concat post with stream', async (t) => {
+    server.once('stream', async (stream, headers) => {
+      t.equal(headers[':method'], 'POST')
+
+      const body = await getStream(stream)
+
+      t.equal(body, 'this is a post!')
+
+      stream.respond({ ':status': 200 })
+      stream.end('hello world')
+    })
+
+    const res = await h2url.concat({
+      url,
+      method: 'POST',
+      body: new Readable({
+        read() {
+          this.push('this is a post!')
+          this.push(null)
+        }
+      })
+    })
+
+    t.equal(res.body, 'hello world')
+    t.equal(res.headers[':status'], 200)
+  })
+
+  test('request', async (t) => {
+    server.once('stream', (stream, headers) => {
+      t.equal(headers[':method'], 'GET')
+      stream.respond({ ':status': 200 })
+      stream.end('hello world')
+    })
+
+    const res = await h2url.request({ url })
+
+    t.ok(res.stream instanceof Writable)
+    t.equal(res.headers[':status'], 200)
+
+    const body = await getStream(res.stream)
+
+    t.equal(body, 'hello world')
+  })
+
+  test('concat request receiving headers', async (t) => {
+    server.once('stream', (stream, headers) => {
+      t.equal(headers[':method'], 'GET')
+      stream.respond({ ':status': 200, 'content-type': 'application/json' })
+      stream.end(JSON.stringify({ hello: 'world' }))
+    })
+
+    const res = await h2url.concat({ url })
+
+    t.deepEqual(JSON.parse(res.body), { hello: 'world' })
+    t.equal(res.headers[':status'], 200)
+    t.equal(res.headers['content-type'], 'application/json')
+  })
+
+  test('concat post', async (t) => {
+    server.once('stream', async (stream, headers) => {
+      t.equal(headers[':method'], 'POST')
+      t.equal(headers['content-type'], 'application/json')
+
+      const body = await getStream(stream)
+
+      t.deepEqual(JSON.parse(body), { hello: 'world' })
+
+      stream.respond({ ':status': 200 })
+      stream.end('hello world')
+    })
+
+    const res = await h2url.concat({
+      url,
+      method: 'POST',
+      body: JSON.stringify({ hello: 'world' }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+
+    t.equal(res.body, 'hello world')
+    t.equal(res.headers[':status'], 200)
+  })
+}
