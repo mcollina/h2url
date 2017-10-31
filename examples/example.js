1768e2d266870c7901f08860ab85358ea704185c
phra <greensoncio@gmail.com>
2017-10-31 20:46:16 +0100

    refactor: remove stream from google get example

--- examples/example.js
+++ examples/example.js
@@ -2,7 +2,6 @@
 
 require('make-promises-safe')
 const h2url = require('..')
-const getStream = require('get-stream')
 
 const url = 'https://google.com'
 
@@ -13,19 +12,3 @@ async function concat () {
 }
 
 concat()
-
-async function streaming () {
-  const res = await h2url.request({
-    method: 'GET',
-    headers: {
-      // 'content-type': 'application/json'
-    }// ,
-    // body: JSON.stringify('something') // string, buffer or readable stream
-  })
-
-  console.log(res.headers)
-  const body = getStream(res.stream)
-  console.log(body)
-}
-
-streaming()
